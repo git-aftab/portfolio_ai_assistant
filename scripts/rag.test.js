6eb69cd14565580fbadd85cf8d5d18d3dfaaeff8
aftab <mdaftab0726@gmail.com>
2026-07-15 20:24:37 +0530

    tested the the query embedding and qdrant results

--- scripts/rag.test.js
+++ scripts/rag.test.js
@@ -1,4 +1,20 @@
-import { approximateTokens, chunkText, cleanTranscript } from "../src/services/chunking";
-import {generateEmbedding} from "../src/services/embedding.js"
-import {storeEmbeddings, searchEmbedding} from "../src/services/qdrant.js"
+// import {
+//   approximateTokens,
+//   chunkText,
+//   cleanTranscript,
+// } from "../src/services/chunking";
+import { generateEmbedding } from "../src/services/embedding.service.js";
+import { searchEmbedding } from "../src/services/qdrant.service.js";
 
+const embedding = await generateEmbedding(
+  "Tell me about VideoTube",
+  "retrieval.query",
+);
+
+const results = await searchEmbedding(embedding);
+
+// console.log(JSON.stringify(results,null, 2));
+console.dir(results, {
+  depth: null,
+  colors: true,
+});
