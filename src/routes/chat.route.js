d3be03c02a002dbae7eebd7cf5c1eb8929951301
aftab <mdaftab0726@gmail.com>
2026-08-02 16:28:21 +0530

    added the route of analytics

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -1,9 +1,9 @@
-import { queryLLM } from "../controllers/chat.controller.js";
+import { queryLLM, getAnalytics } from "../controllers/chat.controller.js";
 import { Router } from "express";
 
 const router = Router();
 
 router.route("/query-llm").post(queryLLM);
-
+router.route("/analytics").get(getAnalytics);
 
 export default router;
